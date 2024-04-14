24c37fffefce414f5a2dae6090fbf8b98f807c5c
parthsolanke <parthsolanke2002@gmail.com>
2024-04-14 20:38:44 +0530

    feat: update with image_utils usage

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 import cv2
 import numpy as np
 import mediapipe as mp
-import utils.detector as dtr
+import utils.detector_utils as dtu
+import utils.image_utils as imu
 
-# Create an instance of the FaceDetector class
-face_detector = dtr.FaceDetector(model_asset_path='./utils/weights/detector.tflite')
+VIDEO_PATH = 0  # Webcam
+SAVE_DIR = "./dynamic/data"
+WEIGHTS_PATH = "./utils/weights/detector.tflite"
 
-video_path = 0 # Set to 0 to use the webcam
-cap = cv2.VideoCapture(video_path)
+face_detector = dtu.FaceDetector(model_asset_path=WEIGHTS_PATH)
+image_manager = imu.ImageManager(save_dir=SAVE_DIR)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+cap = cv2.VideoCapture(VIDEO_PATH)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 
-    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-    detection_result = face_detector.detector.detect(image)
-    image_copy = np.copy(frame)
-    annotated_image = face_detector.visualize(
-        image_copy,
-        detection_result,
-        show_keypoints=False,
-        show_label_score=False
-    )
+image_manager.clear_directory()
+image_manager.create_directory()
 
-    cv2.imshow("Face Detection", annotated_image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+try:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        detection_result = face_detector.detector.detect(image)
+        image_copy = np.copy(frame)
+        annotated_image = face_detector.visualize(
+            image_copy,
+            detection_result,
+            show_keypoints=False,
+            show_label_score=False
+        )
+
+        cv2.imshow("Face Detection", annotated_image)
+
+        # Check for key press events
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+        elif key == ord('c'):
+            image_manager.capture_image(frame)
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
